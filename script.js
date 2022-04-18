4746ac3560891a51a1f614be0ab9bb4912256e09
SoumyaSamriddhi <78063739+SoumyaSamriddhi@users.noreply.github.com>
2022-04-18 20:28:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var myapp = angular.module("myList",[]);
+myapp.controller("myListController",function($scope){
+    $scope.items = ["AngularJS", "ReactJS", "UnderscoreJS"];
+    $scope.newItem  = "";
+    $scope.pushItem = function () {
+        if($scope.newItem != ""){
+        $scope.items.push($scope.newItem);
+        $scope.newItem = "";
+        }
+    }
+
+    $scope.deleteItem = function(index) {
+        $scope.items.splice(index, 1);
+
+    }
+});
